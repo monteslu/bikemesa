717388511061fe3e0dd60b1ef80dce0d855439a6
Luis Montes <monteslu@gmail.com>
2019-07-06 13:01:51 -0700

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Mesa Bicycle Action Group`,
+    description: `A group of bicycle enthusiasts working to make bicycling in Mesa more enjoyable.`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>A group of bicycle enthusiasts working to make bicycling in Mesa more enjoyable.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
